7f4c7d8b874894a9a2ebfcf87f0419e566a300c3
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-09 14:37:31 +0600

    Python-Decorator-Concepts

--- Section-19 Python Decorators/1.Decorator-Basic-Concept.py
+++ Section-19 Python Decorators/1.Decorator-Basic-Concept.py
@@ -4,5 +4,54 @@
     Decorator is a function that accepts a function and returns 
     a function.
     
-    For Example: Consider a program
+    For Example: Consider a the following program
+    >>> def is_prime(passed_number):
+            for num in range(2, passed_number//2):
+                if passed_number % n == 0:
+                    return False
+            else:
+                return True 
+    >>> is_prime(52525235) # returns some result after a while
+    
+    Now, when we call the same function with the same number it again
+    perform the calculation and then returns with a result after some
+    time.
+    >>> is_prime(52525235)
+    
+    We would like to speed up the calculation process of is_prime() function
+    and for that we can use Python built-in module 'functools' one decorator
+    'lru_cache'. 'lru_cache' decorator accepts a function and returns a new 
+    function that wraps around the original function:
+        >>> from functools import lru_cache
+        >>> is_prime_number = lru_cache(is_prime)
+        
+    The 'lru_cache' decorator returned a new function object to which we're 
+    pointing our 'is_prime_number' variable to. When we call this new function, 
+    the new function calls our original is_prime() function (which we had passed 
+    to lru_cache) and it caches the return value for each argument that it sees.
+
+    Every time this new function is called, it stores the inputs (the given function 
+    arguments) and the output (return value) that corresponds to those inputs.
+    
+    This way of using decorator is bit unusual instead to use a decorator we
+    use a special symbol like- @ to define the decorator.
+    
 '''
+
+# importing the 'lru_cache' from functools
+from functools import lru_cache
+
+# defining the decorator
+@lru_cache
+def is_prime(passed_number):
+    for n in range(2, passed_number // 2 ):
+        if passed_number % n == 0: # not a prime number condition
+            return False
+    else: # its a prime number
+        return True
+
+# checking the is_prime() function will see returns a function object 
+# basically nothing but a callable object. 
+print(is_prime) # returns a callable object <functools._lru_cache_wrapper object at 0x7f735dc8fc10>
+is_prime_number = is_prime(2242452)
+print(is_prime_number)
---
+++ Section-19 Python Decorators/2.Decorator-Function-Inside-A-Class-Concept.py
@@ -0,0 +1,46 @@
+'''
+    Decorator Functions Or Decorator Class
+    ======================================
+    A decorator can be a class which accepts a function and returns an instance  
+    of that class (a "decorator class" that can be used as a "function decorator").
+
+    A decorator can also be a function which accepts a class 
+    (a "decorator function" that can be used as a "class decorator").
+    
+    For Example-
+    >>> class ClassName:
+            def __init__(self, val):
+                self.val = val
+            @property
+            def function_name(self):
+                return val
+                
+    That 'property' decorator accepted a function but it didn't return a 
+    function, it returned a property object instead-
+        >>> ClassName.function_name # <property object at 0x000001CED0DC1360>
+        
+    The property decorator that's built-in to Python is actually a class.
+        >>> property # <class 'property'>
+        
+    The built-in property decorator is a class which accepts a function 
+    (the function_name() method in above case) and returns an instance 
+    of the property class.
+
+    So,Function decorator (a decorator used for decorating functions) isn't a 
+    decorator function so much as a decorator class (meaning it's implemented 
+    using a class under the hood). But the thing we care about is that it's 
+    a callable that accepts a function, so we can use it the same way as 
+    any other decorator.
+
+'''
+
+class Square:
+    def __init__(self, width) -> None:
+        self.width = width
+    # defining the decorator
+    @property # property decorator accepted a function and will return property object
+    def find_area(self):
+        return self.width ** 2
+    
+print(Square) # returns a class object
+print(Square.find_area) # return a class property decorator object
