62ff461ab7ef5f2625d0f684b6b271b22dbcb894
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-09 13:59:59 +0600

    Python-Decorators-Concept

---
+++ Section-19 Python Decorators/1.Decorator-Basic-Concept.py
@@ -0,0 +1,8 @@
+'''
+    Decorator Basic Concept
+    =======================
+    Decorator is a function that accepts a function and returns 
+    a function.
+    
+    For Example: Consider a program
+'''
